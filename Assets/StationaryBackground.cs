9548dfe751650aeab43544a9cfb1e96fe53f4e87
kadynCBR <kadyn_martinez@hotmail.com>
2020-08-18 01:30:19 -0700

    mirror included now

---
+++ Assets/StationaryBackground.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationaryBackground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0,0,5);
+        transform.localScale = Camera.main.transform.localScale;
+    }
+}
