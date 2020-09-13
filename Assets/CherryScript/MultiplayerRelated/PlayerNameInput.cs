5b50c0a16541642d0cbf6f74cc9ca50812aaeb7a
kadynCBR <kadyn_martinez@hotmail.com>
2020-09-13 12:35:57 -0700

    multiplayer start

---
+++ Assets/CherryScript/MultiplayerRelated/PlayerNameInput.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Multiplayer.Lobby
+{
+    public class PlayerNameInput : MonoBehaviour
+    {
+        [Header("UI")]
+        [SerializeField] private TMP_InputField nameInputField = null;
+        [SerializeField] private Button continueButton = null;
+
+        public static string DisplayName {get; private set; }
+        private const string PlayerPrefsNameKey = "PlayerName";
+
+        // Start is called before the first frame update
+        private void Start() => SetUpInputField();
+
+        private void SetUpInputField()
+        {
+            if (!PlayerPrefs.HasKey(PlayerPrefsNameKey)) {return; }
+            string defaultName = PlayerPrefs.GetString(PlayerPrefsNameKey);
+
+            nameInputField.text = defaultName;
+            SetPlayerName(defaultName);
+        }
+
+        public void SetPlayerName(string name)
+        {
+            continueButton.interactable = !string.IsNullOrEmpty(name);
+        }
+
+        public void SavePlayerName()
+        {
+            DisplayName = nameInputField.text;
+            PlayerPrefs.SetString(PlayerPrefsNameKey, DisplayName);
+        }
+    }
+}
---
+++ Assets/CherryScript/MultiplayerRelated/RoomPlayerLobby.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+namespace Multiplayer.Lobby
+{
+    public class RoomPlayerLobby : NetworkBehaviour
+    {
+
+    }
+}
